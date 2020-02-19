da4e0c2136ddf8c8c9ad657459e7a784b47e0327
Vladimir <vladimir.chubenko91@gmail.com>
2020-02-19 17:17:00 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {connect} from 'react-redux'
+import first from './assets/first.png'
 
-function App() {
+function App(props) {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="field">
+        <div><img src="assets/1.png" alt=""/> {props.field.first.value}</div>
+        <div><img src="/assets/1.png" alt=""/>{props.field.second.value}</div>
+        <div><img src="./assets/1.png" alt=""/>{props.field.third.value}</div>
+        <div><img src={first} alt="first"/>{props.field.fourth.value}</div>
+        <div>{props.field.first.value}</div>
+        <div>{props.field.first.value}</div>
+        <div>{props.field.first.value}</div>
+        <div>{props.field.first.value}</div>
+        <div>{props.field.first.value}</div>
+      </div>
+
     </div>
   );
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    whatever: state.whatever,
+    field: state.field
+  }
+}
+
+export default connect(mapStateToProps)(App);
