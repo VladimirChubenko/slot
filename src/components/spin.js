c6c40f34cfffab702c2db9e845d3186553c40adf
Volodymyr Chubenko <imac@MacBook-Pro-Volodymyr.local>
2020-03-04 21:43:12 +0200

    react-transition-group

--- src/components/spin.js
+++ src/components/spin.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import spin from '../assets/spin.gif'
+import {CSSTransition} from 'react-transition-group'
 
 export function Spin() {
   return (
-    <div className="Spin">
-      <img  src={spin} alt="spin"/>
-    </div>
+    <CSSTransition>
+      <div className="Spin">
+        <img  src={spin} alt="spin"/>
+      </div>
+    </CSSTransition>
   )
 } 
